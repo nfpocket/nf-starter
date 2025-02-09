9c924c4b304d4ef3e1554f71b959f8a879778181
Norman F <nf@pocket-rocket.io>
2025-02-09 16:50:14 +0100

    refactor: restructure app layout and configuration

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <UButton
+      :label="`Count: ${count}`"
+      @click="count++"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const count = ref(0);
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui"],
+  future: {
+    compatibilityVersion: 4,
+  },
+  srcDir: "app/",
+  serverDir: "server/",
+});
